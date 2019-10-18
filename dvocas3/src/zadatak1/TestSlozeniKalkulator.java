f5c421085b2f532b81401e730cab3fcf1db3d2e0
bojantomic <tomicb@fon.rs>
2019-10-18 17:52:23 +0200

    Revert "Obrisana resenja"
    This reverts commit d2b9bcac3ad59ea2bfa1dce758ac58b35bf9c56f.

---
+++ dvocas3/src/zadatak1/TestSlozeniKalkulator.java
@@ -0,0 +1,15 @@
+package zadatak1;
+
+public class TestSlozeniKalkulator {
+
+	public static void main(String[] args) {
+		
+		SlozeniKalkulator sk = new SlozeniKalkulator();
+		
+		sk.ispisi1Do100();
+		
+		SlozeniKalkulator.deliSaDva(33);
+
+	}
+
+}
