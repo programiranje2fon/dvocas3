23b7fd18a6ac143a77a3ae3ec030e6eae0e8bbc6
PetarLakcevic <petar.lakcevic@gmail.com>
2025-10-26 21:56:10 +0100

    Eclipse -> Intellij

---
+++ src/zadatak2/TestSlozeniKalkulator2.java
@@ -0,0 +1,15 @@
+package zadatak2;
+
+public class TestSlozeniKalkulator2 {
+
+	public static void main(String[] args) {
+
+		int faktorijel;
+		faktorijel = SlozeniKalkulator2.faktorijel(5);
+		System.out.println("Faktorijel 5 je: "+faktorijel);
+		
+		int abn = SlozeniKalkulator2.ABN(2, 3, 2);
+		System.out.println("(2+3)^2="+abn);
+	}
+
+}
